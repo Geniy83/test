7fd0d238b76e3a32d92f90745d31165ae5828da9
Geniy83 <3477734n@gmail.com>
2021-10-20 16:17:45 +0300

    commit v2.5

--- src/test/java/com/mastery/java/task/service/EmployeeServiceTest.java
+++ src/test/java/com/mastery/java/task/service/EmployeeServiceTest.java
@@ -1,11 +1,57 @@
 package com.mastery.java.task.service;
 
+import com.mastery.java.task.dao.EmployeeDao;
+import com.mastery.java.task.dao.EmployeeDaoImpl;
+import com.mastery.java.task.dto.Employee;
+import com.mastery.java.task.dto.Gender;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import javax.xml.crypto.Data;
+import java.sql.Date;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+//@SpringBootTest
 public class EmployeeServiceTest {
 
+    @Mock
+    private EmployeeDaoImpl employeeDao;
+
+    private EmployeeServiceImpl employeeService;
+
+    private static  Employee testEmployee;
+
+    @BeforeClass
+    public void prepareTestData() {
+        Employee testEmployee = new Employee();
+        testEmployee.setEmployeeId(1L);
+        testEmployee.setFirstName("Tom");
+        testEmployee.setLastName("Cruz");
+        testEmployee.setDepartmentID(1);
+        testEmployee.setJobTitle("engineer");
+        testEmployee.setGender(Gender.valueOf("MALE"));
+        testEmployee.setDataOfBirth(Date.valueOf("1983-02-03"));
+        }
+
+    @Before
+    public void init() {
+        employeeService = new EmployeeServiceImpl(employeeDao);
+        }
+    @Test
+    public void updateTest() {
+//        when(employeeDao.getEmployeeId(any(Long.class))).thenReturn(testEmployee);
+//        when(employeeDao.update(any(Employee.class))).;
+        Mockito.verify(employeeDao, Mockito.times(1));
+    }
 }
